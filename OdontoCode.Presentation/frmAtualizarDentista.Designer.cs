0e67b1411d16d18a3fa6d73c9d91a4856a3d1ffb
Marcella R Pastore <marcella.rpastore@gmail.com>
2022-08-19 17:20:05 -0300

    Feito Frm AtualizarCadastroDentista

--- OdontoCode.Presentation/frmAtualizarDentista.Designer.cs
+++ OdontoCode.Presentation/frmAtualizarDentista.Designer.cs
@@ -28,21 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblNomeDentista = new System.Windows.Forms.Label();
+            this.lblAtualizarDentista = new System.Windows.Forms.Label();
+            this.lablCroDentista = new System.Windows.Forms.Label();
+            this.lblTelefoneDentista = new System.Windows.Forms.Label();
+            this.txtNomeDentistaAtualizar = new System.Windows.Forms.TextBox();
+            this.txtCroDentistaAtualizar = new System.Windows.Forms.TextBox();
+            this.txtTelefoneDentistaAtualizar = new System.Windows.Forms.TextBox();
+            this.btnAtualizarDentista = new System.Windows.Forms.Button();
+            this.btnEncontrarDentista = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblNomeDentista
+            // 
+            this.lblNomeDentista.AutoSize = true;
+            this.lblNomeDentista.Location = new System.Drawing.Point(66, 190);
+            this.lblNomeDentista.Name = "lblNomeDentista";
+            this.lblNomeDentista.Size = new System.Drawing.Size(61, 20);
+            this.lblNomeDentista.TabIndex = 0;
+            this.lblNomeDentista.Text = "Nome : ";
+            // 
+            // lblAtualizarDentista
+            // 
+            this.lblAtualizarDentista.AutoSize = true;
+            this.lblAtualizarDentista.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAtualizarDentista.Location = new System.Drawing.Point(201, 60);
+            this.lblAtualizarDentista.Name = "lblAtualizarDentista";
+            this.lblAtualizarDentista.Size = new System.Drawing.Size(425, 46);
+            this.lblAtualizarDentista.TabIndex = 1;
+            this.lblAtualizarDentista.Text = "Atualizar Cadastro Dentista";
+            // 
+            // lablCroDentista
+            // 
+            this.lablCroDentista.AutoSize = true;
+            this.lablCroDentista.Location = new System.Drawing.Point(66, 270);
+            this.lablCroDentista.Name = "lablCroDentista";
+            this.lablCroDentista.Size = new System.Drawing.Size(49, 20);
+            this.lablCroDentista.TabIndex = 2;
+            this.lablCroDentista.Text = "CRO : ";
+            // 
+            // lblTelefoneDentista
+            // 
+            this.lblTelefoneDentista.AutoSize = true;
+            this.lblTelefoneDentista.Location = new System.Drawing.Point(400, 271);
+            this.lblTelefoneDentista.Name = "lblTelefoneDentista";
+            this.lblTelefoneDentista.Size = new System.Drawing.Size(77, 20);
+            this.lblTelefoneDentista.TabIndex = 3;
+            this.lblTelefoneDentista.Text = "Telefone : ";
+            // 
+            // txtNomeDentistaAtualizar
+            // 
+            this.txtNomeDentistaAtualizar.Location = new System.Drawing.Point(138, 187);
+            this.txtNomeDentistaAtualizar.Name = "txtNomeDentistaAtualizar";
+            this.txtNomeDentistaAtualizar.Size = new System.Drawing.Size(577, 27);
+            this.txtNomeDentistaAtualizar.TabIndex = 4;
+            // 
+            // txtCroDentistaAtualizar
+            // 
+            this.txtCroDentistaAtualizar.Location = new System.Drawing.Point(138, 267);
+            this.txtCroDentistaAtualizar.Name = "txtCroDentistaAtualizar";
+            this.txtCroDentistaAtualizar.Size = new System.Drawing.Size(232, 27);
+            this.txtCroDentistaAtualizar.TabIndex = 5;
+            // 
+            // txtTelefoneDentistaAtualizar
+            // 
+            this.txtTelefoneDentistaAtualizar.Location = new System.Drawing.Point(483, 268);
+            this.txtTelefoneDentistaAtualizar.Name = "txtTelefoneDentistaAtualizar";
+            this.txtTelefoneDentistaAtualizar.Size = new System.Drawing.Size(232, 27);
+            this.txtTelefoneDentistaAtualizar.TabIndex = 6;
+            // 
+            // btnAtualizarDentista
+            // 
+            this.btnAtualizarDentista.BackColor = System.Drawing.Color.Gold;
+            this.btnAtualizarDentista.Location = new System.Drawing.Point(271, 400);
+            this.btnAtualizarDentista.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAtualizarDentista.Name = "btnAtualizarDentista";
+            this.btnAtualizarDentista.Size = new System.Drawing.Size(219, 44);
+            this.btnAtualizarDentista.TabIndex = 56;
+            this.btnAtualizarDentista.Text = "Atualizar Cadastro do Dentista";
+            this.btnAtualizarDentista.UseVisualStyleBackColor = false;
+            this.btnAtualizarDentista.Visible = false;
+            // 
+            // btnEncontrarDentista
+            // 
+            this.btnEncontrarDentista.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnEncontrarDentista.Location = new System.Drawing.Point(755, 187);
+            this.btnEncontrarDentista.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEncontrarDentista.Name = "btnEncontrarDentista";
+            this.btnEncontrarDentista.Size = new System.Drawing.Size(86, 31);
+            this.btnEncontrarDentista.TabIndex = 57;
+            this.btnEncontrarDentista.Text = "Buscar";
+            this.btnEncontrarDentista.UseVisualStyleBackColor = false;
             // 
             // frmAtualizarDentista
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.btnEncontrarDentista);
+            this.Controls.Add(this.btnAtualizarDentista);
+            this.Controls.Add(this.txtTelefoneDentistaAtualizar);
+            this.Controls.Add(this.txtCroDentistaAtualizar);
+            this.Controls.Add(this.txtNomeDentistaAtualizar);
+            this.Controls.Add(this.lblTelefoneDentista);
+            this.Controls.Add(this.lablCroDentista);
+            this.Controls.Add(this.lblAtualizarDentista);
+            this.Controls.Add(this.lblNomeDentista);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.Name = "frmAtualizarDentista";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmAtualizarDentista";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label lblNomeDentista;
+        private Label lblAtualizarDentista;
+        private Label lablCroDentista;
+        private Label lblTelefoneDentista;
+        private TextBox txtNomeDentistaAtualizar;
+        private TextBox txtCroDentistaAtualizar;
+        private TextBox txtTelefoneDentistaAtualizar;
+        private Button btnAtualizarDentista;
+        private Button btnEncontrarDentista;
     }
 }
