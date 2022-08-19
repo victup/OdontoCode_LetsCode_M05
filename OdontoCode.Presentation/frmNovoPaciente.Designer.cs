349380d8714c6ca94f4d384c596d512a07152b9f
lucashenriquespereira <lucasphenriques@hotmail.com>
2022-08-18 22:23:24 -0300

    Adding itens on new patient form

--- OdontoCode.Presentation/frmNovoPaciente.Designer.cs
+++ OdontoCode.Presentation/frmNovoPaciente.Designer.cs
@@ -28,19 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtNomePaciente = new System.Windows.Forms.TextBox();
+            this.lblNomePaciente = new System.Windows.Forms.Label();
+            this.lblTelPaciente = new System.Windows.Forms.Label();
+            this.txtTelPaciente = new System.Windows.Forms.TextBox();
+            this.lblCepPaciente = new System.Windows.Forms.Label();
+            this.txtRuaPaciente = new System.Windows.Forms.TextBox();
+            this.lblUfPaciente = new System.Windows.Forms.Label();
+            this.lblRuaPaciente = new System.Windows.Forms.Label();
+            this.lblBairroPaciente = new System.Windows.Forms.Label();
+            this.lblCidadePaciente = new System.Windows.Forms.Label();
+            this.lblNumPaciente = new System.Windows.Forms.Label();
+            this.txtCepPaciente = new System.Windows.Forms.TextBox();
+            this.txtBairroPaciente = new System.Windows.Forms.TextBox();
+            this.txtUfPaciente = new System.Windows.Forms.TextBox();
+            this.txtCidadePaciente = new System.Windows.Forms.TextBox();
+            this.txtNumPaciente = new System.Windows.Forms.TextBox();
+            this.lblNovoPaciente = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // frmCadastroPaciente
+            // txtNomePaciente
+            // 
+            this.txtNomePaciente.Location = new System.Drawing.Point(83, 125);
+            this.txtNomePaciente.Name = "txtNomePaciente";
+            this.txtNomePaciente.Size = new System.Drawing.Size(192, 23);
+            this.txtNomePaciente.TabIndex = 0;
+            // 
+            // lblNomePaciente
+            // 
+            this.lblNomePaciente.AutoSize = true;
+            this.lblNomePaciente.Location = new System.Drawing.Point(31, 128);
+            this.lblNomePaciente.Name = "lblNomePaciente";
+            this.lblNomePaciente.Size = new System.Drawing.Size(46, 15);
+            this.lblNomePaciente.TabIndex = 1;
+            this.lblNomePaciente.Text = "Nome :";
+            // 
+            // lblTelPaciente
+            // 
+            this.lblTelPaciente.AutoSize = true;
+            this.lblTelPaciente.Location = new System.Drawing.Point(20, 181);
+            this.lblTelPaciente.Name = "lblTelPaciente";
+            this.lblTelPaciente.Size = new System.Drawing.Size(57, 15);
+            this.lblTelPaciente.TabIndex = 2;
+            this.lblTelPaciente.Text = "Telefone :";
+            // 
+            // txtTelPaciente
+            // 
+            this.txtTelPaciente.Location = new System.Drawing.Point(83, 178);
+            this.txtTelPaciente.Name = "txtTelPaciente";
+            this.txtTelPaciente.Size = new System.Drawing.Size(192, 23);
+            this.txtTelPaciente.TabIndex = 3;
+            // 
+            // lblCepPaciente
+            // 
+            this.lblCepPaciente.AutoSize = true;
+            this.lblCepPaciente.Location = new System.Drawing.Point(43, 232);
+            this.lblCepPaciente.Name = "lblCepPaciente";
+            this.lblCepPaciente.Size = new System.Drawing.Size(34, 15);
+            this.lblCepPaciente.TabIndex = 4;
+            this.lblCepPaciente.Text = "CEP :";
+            // 
+            // txtRuaPaciente
+            // 
+            this.txtRuaPaciente.Location = new System.Drawing.Point(388, 229);
+            this.txtRuaPaciente.Name = "txtRuaPaciente";
+            this.txtRuaPaciente.Size = new System.Drawing.Size(192, 23);
+            this.txtRuaPaciente.TabIndex = 5;
+            // 
+            // lblUfPaciente
+            // 
+            this.lblUfPaciente.AutoSize = true;
+            this.lblUfPaciente.Location = new System.Drawing.Point(50, 292);
+            this.lblUfPaciente.Name = "lblUfPaciente";
+            this.lblUfPaciente.Size = new System.Drawing.Size(27, 15);
+            this.lblUfPaciente.TabIndex = 6;
+            this.lblUfPaciente.Text = "UF :";
+            // 
+            // lblRuaPaciente
+            // 
+            this.lblRuaPaciente.AutoSize = true;
+            this.lblRuaPaciente.Location = new System.Drawing.Point(349, 232);
+            this.lblRuaPaciente.Name = "lblRuaPaciente";
+            this.lblRuaPaciente.Size = new System.Drawing.Size(33, 15);
+            this.lblRuaPaciente.TabIndex = 7;
+            this.lblRuaPaciente.Text = "Rua :";
+            // 
+            // lblBairroPaciente
+            // 
+            this.lblBairroPaciente.AutoSize = true;
+            this.lblBairroPaciente.Location = new System.Drawing.Point(338, 181);
+            this.lblBairroPaciente.Name = "lblBairroPaciente";
+            this.lblBairroPaciente.Size = new System.Drawing.Size(44, 15);
+            this.lblBairroPaciente.TabIndex = 8;
+            this.lblBairroPaciente.Text = "Bairro :";
+            // 
+            // lblCidadePaciente
+            // 
+            this.lblCidadePaciente.AutoSize = true;
+            this.lblCidadePaciente.Location = new System.Drawing.Point(332, 128);
+            this.lblCidadePaciente.Name = "lblCidadePaciente";
+            this.lblCidadePaciente.Size = new System.Drawing.Size(50, 15);
+            this.lblCidadePaciente.TabIndex = 9;
+            this.lblCidadePaciente.Text = "Cidade :";
+            // 
+            // lblNumPaciente
+            // 
+            this.lblNumPaciente.AutoSize = true;
+            this.lblNumPaciente.Location = new System.Drawing.Point(325, 292);
+            this.lblNumPaciente.Name = "lblNumPaciente";
+            this.lblNumPaciente.Size = new System.Drawing.Size(57, 15);
+            this.lblNumPaciente.TabIndex = 10;
+            this.lblNumPaciente.Text = "Número :";
+            // 
+            // txtCepPaciente
+            // 
+            this.txtCepPaciente.Location = new System.Drawing.Point(83, 229);
+            this.txtCepPaciente.Name = "txtCepPaciente";
+            this.txtCepPaciente.Size = new System.Drawing.Size(192, 23);
+            this.txtCepPaciente.TabIndex = 11;
+            // 
+            // txtBairroPaciente
+            // 
+            this.txtBairroPaciente.Location = new System.Drawing.Point(388, 178);
+            this.txtBairroPaciente.Name = "txtBairroPaciente";
+            this.txtBairroPaciente.Size = new System.Drawing.Size(192, 23);
+            this.txtBairroPaciente.TabIndex = 12;
+            // 
+            // txtUfPaciente
+            // 
+            this.txtUfPaciente.Location = new System.Drawing.Point(83, 289);
+            this.txtUfPaciente.Name = "txtUfPaciente";
+            this.txtUfPaciente.Size = new System.Drawing.Size(192, 23);
+            this.txtUfPaciente.TabIndex = 13;
+            // 
+            // txtCidadePaciente
+            // 
+            this.txtCidadePaciente.Location = new System.Drawing.Point(388, 125);
+            this.txtCidadePaciente.Name = "txtCidadePaciente";
+            this.txtCidadePaciente.Size = new System.Drawing.Size(192, 23);
+            this.txtCidadePaciente.TabIndex = 15;
+            // 
+            // txtNumPaciente
+            // 
+            this.txtNumPaciente.Location = new System.Drawing.Point(388, 289);
+            this.txtNumPaciente.Name = "txtNumPaciente";
+            this.txtNumPaciente.Size = new System.Drawing.Size(192, 23);
+            this.txtNumPaciente.TabIndex = 16;
+            // 
+            // lblNovoPaciente
+            // 
+            this.lblNovoPaciente.AutoSize = true;
+            this.lblNovoPaciente.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNovoPaciente.Location = new System.Drawing.Point(141, 54);
+            this.lblNovoPaciente.Name = "lblNovoPaciente";
+            this.lblNovoPaciente.Size = new System.Drawing.Size(333, 37);
+            this.lblNovoPaciente.TabIndex = 17;
+            this.lblNovoPaciente.Text = "Cadastro de novo Paciente";
+            // 
+            // frmNovoPaciente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(628, 450);
-            this.Name = "frmCadastroPaciente";
+            this.Controls.Add(this.lblNovoPaciente);
+            this.Controls.Add(this.txtNumPaciente);
+            this.Controls.Add(this.txtCidadePaciente);
+            this.Controls.Add(this.txtUfPaciente);
+            this.Controls.Add(this.txtBairroPaciente);
+            this.Controls.Add(this.txtCepPaciente);
+            this.Controls.Add(this.lblNumPaciente);
+            this.Controls.Add(this.lblCidadePaciente);
+            this.Controls.Add(this.lblBairroPaciente);
+            this.Controls.Add(this.lblRuaPaciente);
+            this.Controls.Add(this.lblUfPaciente);
+            this.Controls.Add(this.txtRuaPaciente);
+            this.Controls.Add(this.lblCepPaciente);
+            this.Controls.Add(this.txtTelPaciente);
+            this.Controls.Add(this.lblTelPaciente);
+            this.Controls.Add(this.lblNomePaciente);
+            this.Controls.Add(this.txtNomePaciente);
+            this.Name = "frmNovoPaciente";
             this.Text = "frmCadastroPaciente";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox txtNomePaciente;
+        private Label lblNomePaciente;
+        private Label lblTelPaciente;
+        private TextBox txtTelPaciente;
+        private Label lblCepPaciente;
+        private TextBox txtRuaPaciente;
+        private Label lblUfPaciente;
+        private Label lblRuaPaciente;
+        private Label lblBairroPaciente;
+        private Label lblCidadePaciente;
+        private Label lblNumPaciente;
+        private TextBox txtCepPaciente;
+        private TextBox txtBairroPaciente;
+        private TextBox txtUfPaciente;
+        private TextBox txtCidadePaciente;
+        private TextBox txtNumPaciente;
+        private Label lblNovoPaciente;
     }
 }
