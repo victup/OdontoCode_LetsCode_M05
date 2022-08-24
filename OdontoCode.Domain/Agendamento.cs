43fb08bbc83d51e4e9d6b81b4d4cbd9834e25875
Marcella R Pastore <marcella.rpastore@gmail.com>
2022-08-24 20:11:03 -0300

    Funçoes Agendamento

---
+++ OdontoCode.Domain/Agendamento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdontoCode.Domain
+{
+        public class Agendamento
+        {
+            public int Id_agendamento { get; set; }
+            public DateTime Data { get; set; }
+            public string Desc_consulta { get; set; }
+            public int Id_paciente { get; set; }
+            public int Id_dentista { get; set; }
+
+        public Agendamento (int id_agendamento,DateTime data, string desc_consulta,int id_paciente,int id_dentista)
+        {
+            Id_agendamento = id_agendamento;
+            Data = data;
+            Desc_consulta = desc_consulta;
+            Id_paciente = id_paciente;
+            Id_dentista = id_dentista;
+        } 
+
+        public Agendamento()
+        {
+
+        }
+
+
+        }
+}
---
+++ TestesConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using OdontoCode.Services;
+
+class Program
+{
+    static void Main()
+    {
+        PacienteService pacienteService = new PacienteService();
+        //pacienteService.ImprimirPacientes("024.789.698-28");
+        
+    }
+
+}
