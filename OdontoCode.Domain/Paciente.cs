80a933c53ee9f97512e256b092e52debe555cee9
lucashenriquespereira <lucasphenriques@hotmail.com>
2022-08-23 21:25:37 -0300

    criando construtor paciente

--- OdontoCode.Domain/Paciente.cs
+++ OdontoCode.Domain/Paciente.cs
@@ -23,8 +23,26 @@ namespace OdontoCode.Domain
         public int Numero { get; set; }
         public bool Ativo { get; set; } = true;
 
-       
-        
+        public Paciente(int id_paciente, string nome, string cPF, string telefone, string cEP, string uF, string cidade, string bairro, string rua, int numero, bool ativo)
+        {
+            Id_paciente = id_paciente;
+            Nome = nome;
+            CPF = cPF;
+            Telefone = telefone;
+            CEP = cEP;
+            UF = uF;
+            Cidade = cidade;
+            Bairro = bairro;
+            Rua = rua;
+            Numero = numero;
+            Ativo = ativo;
+        }
+
+        public Paciente()
+        {
+
+        }
+
     } 
 
 }
